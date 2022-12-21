eae718cae02c8ff500c9d44c334a3bd950cf2c04
ChykB <malikchika86@gmail.com>
2022-12-21 01:51:36 +0100

    task1

---
+++ if_else_switch/if-else-switch_a.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print lowercase character with putchar
+ *
+ * Return: 0
+*/
+int main()
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+}
