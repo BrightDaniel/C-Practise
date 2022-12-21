3318d635c016825e1db84e3149872cc1bd2e9a46
Sayo8 <oluwasayoadetutu5@gmail.com>
2022-12-21 11:12:49 +0000

    if-else-switch_a.c

---
+++ if-else-switch_a.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *  main - A program that prints alphabet in lowercase
+ *  putchar function only
+ *  Return: Always 0
+ **/
+ 
+int main()
+{
+    	char n;
+       
+	for (n = 'a'; n <='z'; n++) 
+	{
+		putchar(ch);
+	}
+		putchar('\n);
+	     
+		return 0;
+}
