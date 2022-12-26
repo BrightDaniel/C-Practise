b8025bec2b5be894dfd7719b7f1c8ca628177978
ChykB <malikchika86@gmail.com>
2022-12-26 19:40:23 +0100

    Print all the alphabet in lowercase, followed by a new line

--- loops/answers/task1.c
+++ loops/answers/task1.c
@@ -3,6 +3,17 @@
 
 int main()
 {
-	        //TODO
+	        //print all the alphabet in lowercase, followed by a new line
+
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		printf("%c", i);
+	}
+	printf('\n');
+
+
+	return (0);
 		
 }
