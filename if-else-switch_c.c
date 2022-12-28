4b6486d1c222dfb547511dafcc497b100c8ee491
Sayo8 <oluwasayoadetutu5@gmail.com>
2022-12-28 09:18:42 +0000

    if-else-switch_c.c

---
+++ if-else-switch_c.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+	    char name[50];
+	        int score;
+
+		    printf("what is your name? : ");
+		        fgets(name, 50, stdin);
+			    /**scanf("%s", name);
+			     *      * I dicovered that scanf will not allow space to input the second name so i used fgets*/
+			    printf("Welcome %sWhat is your score : ", name);
+			        scanf("%d", &score);
+				    int totalScore = score / 2;
+				        printf("Your score is : %d\n", totalScore);
+
+					    if (totalScore < 50)
+						        {
+								        printf("Sorry you are out\n");
+									    }
+					        else if (totalScore >= 50 && totalScore <= 80)
+							    {
+								            printf("You did well, Unfortunately you'll have to apply for the next cohort\n");
+									        }
+						    else if (totalScore >= 80 && totalScore <= 179)
+							        {
+									        printf("Congratulations, You're through to the next phase\n");
+										    }
+						        else
+								    {
+									            printf("invalid score");
+										        }
+
+							        return (0);
+
+}
