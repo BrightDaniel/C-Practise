c8161a1e9de73c7554856f1812778e342560ebc6
OILEGA <get2mena@gmail.com>
2022-12-30 17:09:44 +0100

    if-else-switch_b.c

--- if_else_switch/if-else-switch_b.c
+++ if_else_switch/if-else-switch_b.c
@@ -3,35 +3,36 @@
 #include <stdio.h>
 
 /**
- * main - function to print out the output numbers
+ * main - assigns random numbers to the variable n
+ * and prints last digit of variable stored in n
+ * @a: last digit of n
  * Return: 0;
  */
 int main(void)
 {
-	        int n, last_digit;
+	        int n;
+		int a;
 
 		srand(time(0));
 	       	n = rand() - RAND_MAX / 2;
 
 	       	/* your code goes there */
 
-		last_digit = n % 10; // last digit of a number
-
-		printf("Last digit of %d is %d \n", n, last_digit);
-		
-		if ( last_digit > 5)
+		a = n % 10; //this is the last digit of n
+			printf("Last digit of %d is  %d\n", n, a); // prints out last digit of n
+		if (a > 5) // if the last digit of n is > 5
 		{
-			printf("and is greater than 5\n")
+			printf(" and is greater than 5\n");
 		}
-		if ( last_digit == 0)
+		else if (a == 0)
 		{
 			printf("and is 0\n");
 		}
-		if (last_digit < 6 && !=0 )
+		else
 		{
-			printf("and is less than 6 and not 0\n");
+			printf(" and is less than 6 and not 0\n");
 		}
+		printf('\n');
 
-
-		 return (0);
+		return (0);
 }
