b8f58674e7585a55b37e95e4901a3af0916b42db
ChykB <malikchika86@gmail.com>
2023-01-05 21:03:59 +0100

    multiplication table

--- nexted_loop/answers/task2.c
+++ nexted_loop/answers/task2.c
@@ -2,5 +2,18 @@
 
 int main()
 {
-	//TODO
+	//multiplication table
+
+
+        int num;
+        int x;
+
+        printf("Enter a number: ");
+        scanf("%d", &num);
+
+        for (x = 1; x <= 12; x++)
+        {
+                printf("%d x %d = %d\n", num, x, num * x);
+        }
+        return (0);
 }
