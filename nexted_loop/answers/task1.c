d5b4575dcab31f20def94cfdc087695aacea7b0e
ChykB <malikchika86@gmail.com>
2023-01-05 21:00:34 +0100

    task 1

--- nexted_loop/answers/task1.c
+++ nexted_loop/answers/task1.c
@@ -2,7 +2,22 @@
 
 int main()
 {
-	//TODO
+	//program to print out the numbers a user 
+	
+	#include <stdio.h>
 
+    int x, y, numRow;
+
+    printf("Enter the number of row you want to print\n");
+    scanf("%d", &numRow);
+
+    for (x = 0; x < numRow; x++)
+    {
+        for (y = 0; y < x; y++)
+        {
+            printf("%d", y);
+        }
+        printf("\n");
+    }
 
 }
